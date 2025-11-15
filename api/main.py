22c24021d010f075309c6e144dc859c6d6425ee3
Aguss <111596201+Agusschajris@users.noreply.github.com>
2025-11-15 20:47:24 -0300

    feat: created main.py to create the graphs and the page.tsx for Uniform Motion

---
+++ api/main.py
@@ -0,0 +1,52 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+import matplotlib.pyplot as plt
+import numpy as np
+import io
+import base64
+from fastapi.middleware.cors import CORSMiddleware
+
+# Initialize FastAPI
+app = FastAPI()
+
+# Allow Naxt.js to connect
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
+
+
+# Data the frontEnd will send
+class UMInputs(BaseModel):
+    x_i: float
+    v: float
+    t_i: float
+    t_f: float
+
+
+# Define POST endpoint that will send the graph
+@app.post("/UM")
+def UM(data: UMInputs):
+    def f_UM(t, t_i, x_i, v):  # UM equation
+        return x_i + v * (t - t_i)
+
+    t = np.linspace(data.t_i, data.t_f, 100)
+    x = f_UM(t, data.t_i, data.x_i, data.v)
+
+    plt.figure()
+    plt.plot(t, x, marker='.', color='red')
+    plt.xlabel("Time")
+    plt.ylabel("Position")
+    plt.title("Uniform Motion")
+    plt.grid(linestyle='dashed')
+
+    # Convert graph to base64 to send it
+    buf = io.BytesIO()
+    plt.savefig(buf, format="png")
+    buf.seek(0)
+    img_base64 = base64.b64encode(buf.read()).decode("utf-8")
+    plt.close()
+
+    return {"image": img_base64}
--- app/uniform-motion/page.tsx
+++ app/uniform-motion/page.tsx
@@ -1,12 +1,26 @@
 export default function UniformMotionPage() {
   return (
-    <main className="p-8">
+    <main className="flex flex-col items-center justify-center min-h-screen">
+      {/* Title */}
       <h1 className="text-3xl font-bold mb-4">
         Uniform Motion (Constant Velocity)
       </h1>
-      <p className="mt-4 text-lg">
-        Here will go the Uniform Motion simulator interface.
+
+      {/* Description */}
+      <p className="mt-4 text-base max-w-prose text-center">
+        "Uniform Motion" is a 1D motion in which an object moves at constant speed and (as Newton helpfully reminds us) will never stop moving unless something stops it. This is the formula we use to calculate the position of the object as a function of time:
       </p>
+
+      {/* Formula */}
+      <p className="mt-4 text-lg font-bold italic max-w-prose text-center">
+        X(t) = X₀ + V·(t - t₀)
+      </p>
+
+      {/* Formula description */}
+      <p className="mt-4 text-base max-w-prose text-center">
+        Where <span className="font-bold italic">X₀</span> is the initial position of the object, <span className="font-bold italic">V</span> is its velocity, and <span className="font-bold italic">t₀</span> is the initial time.
+      </p>
+
     </main>
   );
 }
